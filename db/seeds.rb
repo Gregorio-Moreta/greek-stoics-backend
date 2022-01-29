dc0e4e736804f512ccad238b76f58de494e01f15
Gregorio Moreta <gregoriomoreta4@gmail.com>
2022-01-29 00:59:09 -0500

    rack cors seeded faker done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+20.times do
+    Greek.create(
+        author: Faker::GreekPhilosophers.unique.name,
+        message: Faker::GreekPhilosophers.unique.quote
+    )
+end
+
+puts "Seeded database"
